af1038e7624e6628f1a79455bc7d94bb8392b006
jbc91 <qjsrodkfhd@gmail.com>
2016-11-30 19:22:54 +0900

    프론트 페이지 바꿈 view
    위치 views/buyTogether/buyTogether

--- src/main/java/com/buy/together/controller/BuyTogetherController.java
+++ src/main/java/com/buy/together/controller/BuyTogetherController.java
@@ -10,18 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/front/buyTogether/*")
+@RequestMapping("/buyTogether/*")
 public class BuyTogetherController {
 
 	private static final Logger logger = LoggerFactory.getLogger(BuyTogetherController.class);
 
 	//현재 위치 url 경로 
-	@RequestMapping(value = "/buyTogether", method = RequestMethod.GET)
+	@RequestMapping(value ="/buyTogether", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-
+		
 		System.out.println("buyTogetherController");
 		logger.info("Welcome home! The client locale is {}.", locale);
-
-		return "/front/buyTogether/buyTogether";
+		
+		return "/views/buyTogether/buyTogether";
 	}
 }
